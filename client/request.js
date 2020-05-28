81475f91c44088e9cf747f10331a968040140904
SashaJson <alexander.shulga@teradek.com>
2020-05-28 19:47:17 +0300

    added request.js

---
+++ client/request.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+
+    request: async (url, method = "GET", data = null) => {
+
+        try {
+            const headers = {};
+            let body;
+
+            if (data) {
+                headers['Content-Type'] = "application/json";
+                body = JSON.stringify(data)
+            }
+
+            if (method === "DELETE") {
+                headers['Accept'] = '*/*';
+                headers['Connection'] = 'keep-alive';
+            }
+
+            const response = await fetch(url, {
+                method,
+                headers,
+                body
+            });
+
+            return response.json()
+
+        } catch (err) {
+            console.warn("Error", err.message)
+        }
+
+    }
+}
