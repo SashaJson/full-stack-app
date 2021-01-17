b168b5027d5c4b7bc585355ba646dc4060fba87c
SashaJson <sashamiller666@gmail.com>
2021-01-17 19:38:37 +0200

    changed in client

--- client/frontend.js
+++ client/frontend.js
@@ -1,6 +1,7 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+import request from '../helpers/request'
 
-Vue.component("loader", {
+Vue.component('loader', {
     template: `
 <div style="display: flex; justify-content: center; align-items: center">
     <div class="spinner-border" role="status">
@@ -11,6 +12,7 @@ Vue.component("loader", {
 });
 
 new Vue({
+
     el: "#app",
 
     data() {
@@ -26,84 +28,75 @@ new Vue({
 
     computed: {
         canCreate() {
-            return this.form.value.trim() && this.form.name.trim()
+            return this.form.value.trim() && this.form.name.trim();
         }
     },
 
     methods: {
         async createContact() {
             try {
+
                 const {...contact} = this.form;
 
-                const newContact = await request("http://localhost:3000/api/contacts", "POST", contact);
+                const newContact = await request(
+                    'http://localhost:3000/api/contacts',
+                    'POST',
+                    contact
+                );
 
                 this.contacts.push(newContact);
-                this.form.name = this.form.value = ""
+                this.form.name = this.form.value = '';
 
-            } catch (e) {
-                console.log(e)
+            } catch (error) {
+                console.log(error);
             }
         },
 
         async markContact(id) {
             try {
+
                 const contact = this.contacts.find(c => c.id === id);
-                const updated = await request(`http://localhost:3000/api/contacts/${id}`, "PUT", {
-                    ...contact,
-                    marked: true
-                })
+
+                const updated = await request(
+                    `http://localhost:3000/api/contacts/${id}`,
+                    'PUT', {
+                        ...contact,
+                        marked: true
+                    });
+
                 contact.marked = updated.marked;
-            } catch (e) {
-                console.log(e)
+
+            } catch (error) {
+                console.log(error);
             }
         },
 
         async removeContact(id) {
             try {
-                await request(`http://localhost:3000/api/contacts/${id}`, "DELETE")
-                this.contacts = this.contacts.filter(c => c.id !== id)
-            } catch (e) {
-                console.log(e)
+
+                await request(
+                    `http://localhost:3000/api/contacts/${id}`,
+                    'DELETE'
+                );
+
+                this.contacts = this.contacts.filter(c => c.id !== id);
+
+            } catch (error) {
+                console.log(error);
             }
         }
     },
+
     async mounted() {
         try {
+
             this.loading = true;
-            this.contacts = await request("http://localhost:3000/api/contacts");
+            this.contacts = await request('http://localhost:3000/api/contacts');
             this.loading = false;
-        } catch (e) {
-            console.log(e)
+
+        } catch (error) {
+            console.log(error);
         }
     }
+
 });
-
-async function request(url, method = "GET", data = null) {
-
-    try {
-        const headers = {};
-        let body;
-
-        if (data) {
-            headers['Content-Type'] = "application/json";
-            body = JSON.stringify(data)
-        }
-
-        if (method === "DELETE") {
-            headers['Accept'] = '*/*';
-            headers['Connection'] = 'keep-alive';
-        }
-
-        const response = await fetch(url, {
-            method,
-            headers,
-            body
-        });
-
-        return response.json()
-
-    } catch (err) {
-        console.warn("Error", err.message)
-    }
-
-}
