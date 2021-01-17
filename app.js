2b2b0d569eaca83fcdeac079cf5ecfa05e1e34a3
SashaJson <sashamiller666@gmail.com>
2021-01-17 19:38:15 +0200

    changed file app

--- app.js
+++ app.js
@@ -1,48 +1,40 @@
 'use strict';
 
-const {v4} = require("uuid");
-const path = require("path");
+const {v4} = require('uuid'),
+    path = require('path');
 
-let contacts = [
-    {
-        id: v4(),
-        name: "Sasha Shulga",
-        value: "+380500571658",
-        marked: false
-    }
-];
+let contacts = [];
 
 module.exports = app => {
 
-    //GET
-    app.get("/api/contacts", (req, res) => {
+    app.get('/api/contacts', (req, res) => {
         res.status(200).json(contacts);
     });
 
-    //POST
-    app.post("/api/contacts", (req, res) => {
-        const contact = {...req.body, id: v4(), marked: false}; // need implemented server validation
-        contacts.push(contact)
-        res.status(201).json(contact)
+    // TODO IMPLEMENT SERVER VALIDATION
+
+    app.post('/api/contacts', (req, res) => {
+        const contact = {...req.body, id: v4(), marked: false};
+        contacts.push(contact);
+        res.status(201).json(contact);
     });
 
-    //DELETE
-    app.delete("/api/contacts/:id", (req, res) => {
+    app.delete('/api/contacts/:id', (req, res) => {
         contacts = contacts.filter(c => c.id !== req.params.id);
-        res.status(200).json({message: "Контакт был удален"});
+        res.status(200).json({message: 'Contact has been delete'});
     });
 
-    //PUT
-    app.put("/api/contacts/:id", (req, res) => {
+    app.put('/api/contacts/:id', (req, res) => {
         const ind = contacts.findIndex(c => c.id === req.params.id);
         contacts[ind] = req.body;
-        res.status(200).json(contacts[ind])
+        res.status(200).json(contacts[ind]);
     });
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "index.html"))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
     });
 
-    return app
+    return app;
+
 }
 
